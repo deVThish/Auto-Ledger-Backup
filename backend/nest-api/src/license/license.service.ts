c040ca991e31298e8d3fe5d7820ffca3fd6cc9c3
venushathi <148311558+vthish@users.noreply.github.com>
2026-06-18 08:14:33 +0530

    Change the variable names

--- backend/nest-api/src/license/license.service.ts
+++ backend/nest-api/src/license/license.service.ts
@@ -45,7 +45,7 @@ export class LicenseService {
 
   constructor(private prisma: PrismaService) {
     this.s3Client = new S3Client({
-      region: process.env.AWS_S3_REGION,
+      region: process.env.AWS_REGION,
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -67,7 +67,7 @@ export class LicenseService {
     const uploadUrl = await getSignedUrl(this.s3Client, command, {
       expiresIn: 60,
     });
-    const publicFileUrl = `https://${bucketName}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${uniqueFileName}`;
+    const publicFileUrl = `https://${bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${uniqueFileName}`;
 
     return {
       uploadUrl,
