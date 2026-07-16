20e9c6b966bf435ff733aa4583a75333fc1545bd
venushathi <148311558+vthish@users.noreply.github.com>
2026-07-16 18:00:18 +0530

    Fix the device mismatch not working error

--- frontend_apps/auto_ledger/lib/utils/device_info.dart
+++ frontend_apps/auto_ledger/lib/utils/device_info.dart
@@ -1,16 +1,26 @@
-import 'dart:io';
-import 'package:device_info_plus/device_info_plus.dart';
+import 'dart:math';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class DeviceInfoUtil {
+  static const _storage = FlutterSecureStorage();
+  static const _deviceIdKey = 'app_unique_device_id';
+
   static Future<String> getDeviceId() async {
-    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    if (Platform.isAndroid) {
-      final AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-      return androidInfo.id;
-    } else if (Platform.isIOS) {
-      final IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-      return iosInfo.identifierForVendor ?? 'UNKNOWN_IOS_DEVICE';
+    String? deviceId = await _storage.read(key: _deviceIdKey);
+
+    if (deviceId == null || deviceId.isEmpty) {
+      deviceId = _generateUniqueId();
+      await _storage.write(key: _deviceIdKey, value: deviceId);
     }
-    return 'UNKNOWN_DEVICE';
+
+    return deviceId;
+  }
+
+  static String _generateUniqueId() {
+    final random = Random();
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+    final randomNum =
+        List.generate(8, (_) => random.nextInt(10).toString()).join();
+    return 'DEV-$timestamp-$randomNum';
   }
 }
