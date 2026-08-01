bc1a05d5c31cd9f1e328baa52a87506266a1b068
vthish <148311558+vthish@users.noreply.github.com>
2026-08-02 03:16:33 +0530

    feat(driver): enforce automatic logout when JWT session expires

--- frontend_apps/auto_ledger/lib/utils/secure_storage.dart
+++ frontend_apps/auto_ledger/lib/utils/secure_storage.dart
@@ -1,23 +1,26 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../services/session_service.dart';
 
 class SecureStorage {
   static const _storage = FlutterSecureStorage();
 
-  static Future<void> saveToken(String token) async =>
-      await _storage.write(key: 'jwt_token', value: token);
+  static Future<void> saveToken(String token) async {
+    await _storage.write(key: 'jwt_token', value: token);
+    SessionService.instance.start(token);
+  }
 
   static Future<String?> getToken() async =>
       await _storage.read(key: 'jwt_token');
 
-  static Future<void> deleteToken() async =>
-      await _storage.delete(key: 'jwt_token');
+  static Future<void> deleteToken() async {
+    SessionService.instance.cancel();
+    await _storage.delete(key: 'jwt_token');
+  }
 
   static Future<void> saveNic(String nic) async =>
       await _storage.write(key: 'nic_no', value: nic);
 
-  static Future<String?> getNic() async =>
-      await _storage.read(key: 'nic_no');
+  static Future<String?> getNic() async => await _storage.read(key: 'nic_no');
 
-  static Future<void> deleteNic() async =>
-      await _storage.delete(key: 'nic_no');
+  static Future<void> deleteNic() async => await _storage.delete(key: 'nic_no');
 }
